0397d4be78d9d720bdcfe248da011e06e91f8b5b
Noah Mostacero <noahmostacero@gmail.com>
2015-05-07 11:23:02 -0700

    testing first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+//        configuration of php server
+            set_time_limit(0);
+            ini_set(default_socket_timeout, 300);
+            session_start();
+            
+//            making constants using define
+            
+            define('client_id', b5eb5a06065644139b3669194284658e);
+            define('client_secret', a10f99b5e29946f7822968b0101d21c0);
+            define('client_id', b5eb5a06065644139b3669194284658e);
+            define('client_id', b5eb5a06065644139b3669194284658e);
+            
+        ?>
+        CLIENT INFO
+        CLIENT ID	b5eb5a06065644139b3669194284658e
+        CLIENT SECRET	a10f99b5e29946f7822968b0101d21c0
+        WEBSITE URL	http://localhost/apiapp/index.php
+        REDIRECT URI	http://localhost/apiapp/index.php
+    </body>
+</html>
