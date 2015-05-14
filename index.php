ddc48b2a72aaabc8c9d6cdede9c786b4af42c7d1
Noah Mostacero <noahmostacero@gmail.com>
2015-05-14 12:29:10 -0700

    Milestone 8 completed

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
-
+            //        configuration of php server
+            set_time_limit(0);
+            ini_set('default_socket_timeout', 300);
+            session_start();
+            
+//            making constants using define
+            
+            define('clientID', 'b5eb5a06065644139b3669194284658e');
+            define('clientSecret', 'a10f99b5e29946f7822968b0101d21c0');
+            define('redirectURI', 'http://localhost/apiapp/index.php');
+            define('ImageDirectory', 'pics/');
+            
+            
             if (isset($_GET['code'])){
                 $code = ($_GET['code']);
                 $url = 'https://api.instagram.com/oauth/access_token';
@@ -15,25 +27,14 @@
                 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); //setting it eaul to 1 because we are getting strings back
                 curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); //but in live work-production we want to set this to true
                 
-            }
+            
             
             $result = curl_exec($curl);
-            curl_close();
-            
-//        configuration of php server
-            set_time_limit(0);
-            ini_set('default_socket_timeout', 300);
-            session_start();
-            
-//            making constants using define
-            
-            define('clientID', 'b5eb5a06065644139b3669194284658e');
-            define('client_Secret', 'a10f99b5e29946f7822968b0101d21c0');
-            define('redirectURI', 'http://localhost/apiapp/index.php');
-            define('ImageDirectory', 'pics/');
-            
-            
-            
+            curl_close($curl);
+            $results = json_decode($result, true);
+            echo $results['user']['username'];
+            }
+            else{
             
         ?>
 
@@ -55,23 +56,13 @@ and open the template in the editor.
         <link rel="author" href="humans.txt">-->
     </head>
     <body>
-        <?php
-////        configuration of php server
-//            set_time_limit(0);
-//            ini_set('default_socket_timeout', 300);
-//            session_start();
-//            
-////            making constants using define
-//            
-//            define('clientID', 'b5eb5a06065644139b3669194284658e');
-//            define('client_Secret', 'a10f99b5e29946f7822968b0101d21c0');
-//            define('redirectURI', 'http://localhost/apiapp/index.php');
-//            define('ImageDirectory', 'pics/');
-//            
-//        ?>
+    
         <!--creating a login for people to go and give approval for our app to access there instagram 
             after getting approval we are nwo going to have the information so that we can play with it-->
         <a href="https:api.instagram.com/oauth/authorize/?client_id=<?php echo clientID; ?>&redirect_uri=<?php echo redirectURI ?>&response_type=code">LOGIN</a>
 <!--        <script src="js/main.js"></script>-->
     </body>
 </html>
+<?php
+            }
+            ?>
