8abd9346676159fe166c4557b588804422e6ff37
Noah Mostacero <noahmostacero@gmail.com>
2015-05-14 12:36:09 -0700

    Milestone 9 completed

--- index.php
+++ index.php
@@ -11,6 +11,22 @@
             define('redirectURI', 'http://localhost/apiapp/index.php');
             define('ImageDirectory', 'pics/');
             
+            //Function that is going to connect to instagram
+            function connectToInstagram($url){
+                $ch = curl_init();
+                
+                curl_setopt_array($ch, array(
+                CURLOPT_URL => $url,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_SSL_VERIFYPEER => false, 
+                CURLOPT_SSL_VERIFYHOST => 2,
+                ));
+                $result = curl_exec($ch);
+                curl_close($ch);
+                return $result;
+                        
+            }
+            
             
             if (isset($_GET['code'])){
                 $code = ($_GET['code']);
